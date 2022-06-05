eae5db9d8987040b107b76452bf1c6b5b1d350ef
KentaroMatsuki <95513931+kentaro425@users.noreply.github.com>
2022-06-05 22:18:38 +0900

    sec2

---
+++ MathTraining/QuizViewController.swift
@@ -0,0 +1,62 @@
+//
+//  QuizViewController.swift
+//  MathTraining
+//
+//  Created by 松木健汰郎 on 2022/06/05.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+    
+    let total = 10
+    var correct = 0
+    var questionIndex = 0
+    var answerIndex = 0
+    
+    @IBOutlet var leftNumberLabel: UILabel!
+    @IBOutlet var centerNumberLabel: UILabel!
+    @IBOutlet var rightNumberLabel: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        setQuestions()
+    }
+    
+    @IBAction func tapped(sender: UIButton) {
+        if sender.tag - 1 == answerIndex {
+            correct += 1
+        }
+        
+        questionIndex += 1
+        if questionIndex >= total {
+            performSegue(withIdentifier: "QuizToResult", sender: nil)
+        }
+    }
+
+    func setQuestions() {
+        let leftNum = Int(arc4random_uniform(10))
+        var centerNum = Int(arc4random_uniform(10))
+        
+        answerIndex = Int(arc4random_uniform(4))
+        switch answerIndex {
+        case 0:
+            rightNumberLabel.text = "\(leftNum + centerNum)"
+        case 1:
+            rightNumberLabel.text = "\(leftNum + centerNum)"
+        case 2:
+            rightNumberLabel.text = "\(leftNum + centerNum)"
+        default:
+            if centerNum == 0 {
+                centerNum = 1
+            }
+            rightNumberLabel.text = "\(leftNum / centerNum)"
+        }
+        
+        leftNumberLabel.text = "\(leftNum)"
+        centerNumberLabel.text = "\(centerNum)"
+    }
+
+}
